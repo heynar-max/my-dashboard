4341f57e9523686d2afd5e07aaea1badd1aedd04
heynar-max <heynar76@hotmail.com>
2023-12-23 20:27:31 -0500

    Cargar información del Pokémon por ID

--- src/app/dashboard/pokemon/[id]/page.jsx
+++ src/app/dashboard/pokemon/[id]/page.jsx
@@ -2,7 +2,26 @@
 
 
 
+const getPokemon = async(id) => {
 
+
+    try {
+        const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${ id }`,{
+        cache: 'force-cache'// TODO: cambiar esto en un futuro
+        // next: {
+        //   revalidate: 60 * 60 * 30 * 6
+        // }
+        }).then( resp => resp.json() );
+    
+        console.log('Se cargó: ', pokemon.name);
+    
+        return pokemon;
+        
+    } catch (error) {
+        notFound();
+    }
+
+}
 
 
 
@@ -10,11 +29,14 @@
 export default async function PokemonPage({ params }) {
 
 
-
+const pokemon = await getPokemon (params.id);
 
     return (
         <div>
             <h1>POKEMON {params.id}</h1>
+            <div>
+                {JSON.stringify(pokemon)}
+            </div>
         </div>
     );
 }
