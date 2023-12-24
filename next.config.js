8449a083042b23686e7a9ac81b86be1f50fac579
heynar-max <heynar76@hotmail.com>
2023-12-23 20:50:22 -0500

    Pantalla del Pokemon

--- next.config.js
+++ next.config.js
@@ -6,7 +6,12 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname:'images.unsplash.com'
-            }
+            },
+        
+            {
+                protocol: 'https',
+                hostname:'raw.githubusercontent.com'
+            },
         ]
     }
 }
